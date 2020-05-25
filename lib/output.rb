ecf496cd087874ad8d7ca24e72ce497a4acbc3df
rindra josia <rrindrajosia@gmail.com>
2020-05-25 15:14:03 +0300

    add instruction create  file ../lib/search_result.json before scraping webpage

--- lib/output.rb
+++ lib/output.rb
@@ -1,11 +1,9 @@
 # rubocop:disable Security/Open
 module Output
   def self.check(houses, result_file = '../lib/search_result.json')
-    if File.exist?(result_file)
+    if File.exist?(result_file) && !File.zero?(result_file)
       old_results = JSON.parse(open(result_file).read)
       compare_results(old_results, houses)
-    else
-      File.new(result_file, 'w')
     end
 
     File.open(result_file, 'w') do |file|
@@ -17,7 +15,9 @@ module Output
     houses.each do |house|
       old = old_results.find { |old_result| house[:id] == old_result['id'] }
 
-      warning(house, old) if house[:price] != old['price']
+      unless old.nil?
+        warning(house, old) if house[:price] != old['price']
+      end
     end
   end
 end
